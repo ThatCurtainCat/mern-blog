1c9389893b9319613c3c08fccac1f316a45fb535
ThatCurtainCat <anm37590@gmail.com>
2024-02-03 14:32:52 -0800

    add dashUsers page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  // getUser,
+  getUsers,
   deleteUser,
   signout,
   test,
@@ -13,4 +15,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
+// router.get("/:userId", getUser);
 export default router;
