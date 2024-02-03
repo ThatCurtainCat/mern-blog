0ddfe65c95dcd1ae65cddf13aea9eb2ac9419f95
ThatCurtainCat <anm37590@gmail.com>
2024-02-02 20:46:51 -0800

    create getposts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -3,14 +3,14 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   create,
   // deletepost,
-  // getposts,
+  getposts,
   // updatepost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-// router.get("/getposts", getposts);
+router.get("/getposts", getposts);
 // router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 // router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
